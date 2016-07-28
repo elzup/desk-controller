91d6b5e338764b3f13c687b47db6763e4d289edf
elzup <guild0105@gmail.com>
2016-07-28 18:51:12 +0900

    Add Realtime chart renderring

--- script.js
+++ script.js
@@ -1,10 +1,80 @@
-var manager;
+var manager, chart, data;
 
-window.onload = function() {
+$(function () {
+    data = [];
+    $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=usdeur.json&callback=?', function (data) {
+
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'container',
+                zoomType: 'x'
+            },
+            title: {
+                text: 'USD to EUR exchange rate over time'
+            },
+            subtitle: {
+                text: document.ontouchstart === undefined ?
+                        'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+            },
+            xAxis: {
+                type: 'datetime'
+            },
+            yAxis: {
+                title: {
+                    text: 'Exchange rate'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+            },
+
+            series: [{
+                type: 'area',
+                name: 'USD to EUR',
+                data: []
+            }]
+        });
+    });
+
     manager = (new AudioManager({
         useMicrophone   : true,
         onEnterFrame    : function() {
-            console.log(Utils.sum(this.analysers.mic.getByteFrequencyData()));
+            data.push(Utils.sum(this.analysers.mic.getByteFrequencyData()))
+            if (chart == null) {
+                return;
+            }
+            if (data.length > 12) {
+                data.shift();
+                data.shift();
+            }
+            chart.series[0].setData(data);
+            chart.redraw()
         }
     })).init();
-}
+});
