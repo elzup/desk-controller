1d90d0162535a744095b864d548da817b5e97602
elzup <guild0105@gmail.com>
2016-07-28 23:29:25 +0900

    Refactor: JS, HTML formats

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 var manager, chart, data, series;
 
 $(function () {
-    data = new Array(500);
-    for (var i=0; i < 500; ++i) {
-        data[i] = 0;
-    }
-    $.getJSON('/highchart-config.json', [], function(options) {
-      chart = new Highcharts.Chart(options);
-      series = chart.series[0];
-      series.setData(data, true);
-    });
+  data = new Array(500);
+  for (var i = 0; i < 500; ++i) {
+    data[i] = 0;
+  }
+  $.getJSON('/highchart-config.json', [], function (options) {
+    chart = new Highcharts.Chart(options);
+    series = chart.series[0];
+    series.setData(data, true);
+  });
 
-    manager = (new AudioManager({
-        useMicrophone   : true,
-        onEnterFrame    : function() {
-            // avg of 1024 data
-            data.push(Utils.sum(this.analysers.mic.getByteFrequencyData()) / 1024);
-            if (chart == null) {
-                return;
-            }
-            // NOTE: length が変わらないと redraw が効かないので 2つずつ 変動させている
-            // 500 data Queue
-            if (data.length > 500) {
-                data.shift();
-                data.shift();
-            }
-            series.setData(data, true);
-        }
-    })).init();
+  manager = (new AudioManager({
+    useMicrophone: true,
+    onEnterFrame: function () {
+      // avg of 1024 data
+      data.push(Utils.sum(this.analysers.mic.getByteFrequencyData()) / 1024);
+      if (chart == null) {
+        return;
+      }
+      // NOTE: length が変わらないと redraw が効かないので 2つずつ 変動させている
+      // 500 data Queue
+      if (data.length > 500) {
+        data.shift();
+        data.shift();
+      }
+      series.setData(data, true);
+    }
+  })).init();
 });
