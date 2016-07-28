3045c43d9df40891f753275d4d02f79cd8917f90
elzup <guild0105@gmail.com>
2016-07-29 02:22:55 +0900

    Refactor: For eslint

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
-var $ = require('jquery');
-var Highcharts = require('highcharts');
-var manager, chart, data, series;
+'use strict'
+var $ = require('jquery')
+var Highcharts = require('highcharts')
+
+var chart, data, series
 
 $(function () {
-  data = new Array(500);
+  data = new Array(500)
   for (var i = 0; i < 500; ++i) {
-    data[i] = 0;
+    data[i] = 0
   }
-  $.getJSON('/highchart-config.json', [], function (options) {
-    chart = new Highcharts.Chart(options);
-    series = chart.series[0];
-    series.setData(data, true);
-  });
 
-  manager = (new AudioManager({
+  var fetchJSON = function (url) {
+    return new Promise((resolve, reject) => {
+      $.getJSON(url)
+        .done((json) => resolve(json))
+        .fail((xhr, status, err) => reject(status + err.message));
+    })
+  }
+
+  chart = new Highcharts.Chart(fetchJSON('/highchart-config.json'))
+  series = chart.series[0]
+  series.setData(data, true)
+  var am = new AudioManager({
     useMicrophone: true,
     onEnterFrame: function () {
       // avg of 1024 data
-      data.push(Utils.sum(this.analysers.mic.getByteFrequencyData()) / 1024);
+      data.push(Utils.sum(this.analysers.mic.getByteFrequencyData()) / 1024)
       if (chart == null) {
-        return;
+        return
       }
       // NOTE: length が変わらないと redraw が効かないので 2つずつ 変動させている
       // 500 data Queue
       if (data.length > 500) {
-        data.shift();
-        data.shift();
+        data.shift()
+        data.shift()
       }
-      series.setData(data, true);
+      series.setData(data, true)
     }
-  })).init();
-});
+  })
+  am.init()
+})
