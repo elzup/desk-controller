0a74609397b9769f029281c087f5f23648c4eba8
elzup <guild0105@gmail.com>
2016-07-28 17:23:39 +0900

    Add npm install highchart

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 var manager;
 
-var k;
 window.onload = function() {
-    k = 0;
     manager = (new AudioManager({
         useMicrophone   : true,
         onEnterFrame    : function() {
-            if (k++ == 10) {
-                k = 0;
-                console.log(Utils.sum(this.analysers.mic.getByteFrequencyData()));
-            }
+            console.log(Utils.sum(this.analysers.mic.getByteFrequencyData()));
         }
     })).init();
 }
