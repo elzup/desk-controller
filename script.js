fceabd155928453f96fb1148e31323026762d89d
elzup <guild0105@gmail.com>
2016-07-29 15:25:30 +0900

    Refactor es allow

--- script.js
+++ script.js
@@ -1,43 +1,40 @@
 'use strict'
-var $ = require('jquery')
-var Highcharts = require('highcharts')
+const $ = require('jquery')
+const Highcharts = require('highcharts')
 
-var chart, data, series
+let chart, data, series
 
-$(() => {
-  data = new Array(500)
-  data.fill(0)
+data = Array.form(Array(500), x => 0)
 
-  var fetchJSON = function (url) {
-    return new Promise((resolve, reject) => {
-      $.getJSON(url)
-        .done((json) => resolve(json))
-        .fail((xhr, status, err) => reject(status + err.message))
-    })
-  }
-
-  fetchJSON('/highchart-config.json').then((options) => {
-    chart = new Highcharts.Chart(options)
-    series = chart.series[0]
-    series.setData(data, true)
+var fetchJSON = function (url) {
+  return new Promise((resolve, reject) => {
+    $.getJSON(url)
+      .done((json) => resolve(json))
+      .fail((xhr, status, err) => reject(status + err.message))
   })
+}
 
-  var am = new AudioManager({
-    useMicrophone: true,
-    onEnterFrame: function () {
-      // avg of 1024 data
-      data.push(Utils.sum(this.analysers.mic.getByteFrequencyData()) / 1024)
-      if (chart == null) {
-        return
-      }
-      // NOTE: length が変わらないと redraw が効かないので 2つずつ 変動させている
-      // 500 data Queue
-      if (data.length > 500) {
-        data.shift()
-        data.shift()
-      }
-      series.setData(data, true)
-    }
-  })
-  am.init()
+fetchJSON('/highchart-config.json').then((options) => {
+  chart = new Highcharts.Chart(options)
+  series = chart.series[0]
+  series.setData(data, true)
 })
+
+var am = new AudioManager({
+  useMicrophone: true,
+  onEnterFrame: function () {
+    // avg of 1024 data
+    data.push(Utils.sum(this.analysers.mic.getByteFrequencyData()) / 1024)
+    if (!chart) {
+      return
+    }
+    // NOTE: length が変わらないと redraw が効かないので 2つずつ 変動させている
+    // 500 data Queue
+    if (data.length > 500) {
+      data.shift()
+      data.shift()
+    }
+    series.setData(data, true)
+  }
+})
+am.init()
